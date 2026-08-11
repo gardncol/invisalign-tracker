7f9dbea64ab721675f29e292601e9c24051a7459
Colin <dev@invisalign-tracker.local>
2026-08-11 12:36:07 -0700

    chore: add metro config, web deps, worklets, and successful Android APK build

---
+++ metro.config.js
@@ -0,0 +1,18 @@
+/** @type {import('expo/metro-config').MetroConfig} */
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+// Cross-origin isolation headers needed for expo-sqlite SharedArrayBuffer on web
+config.server = {
+  ...config.server,
+  enhanceMiddleware: (middleware) => {
+    return (req, res, next) => {
+      res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+      res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+      middleware(req, res, next);
+    };
+  },
+};
+
+module.exports = config;
